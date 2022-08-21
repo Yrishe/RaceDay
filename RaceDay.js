84b51c647a029f4e4dd276041b3664595fe33d41
Yarli Rabelo <86082317+Yrishe@users.noreply.github.com>
2022-08-21 01:00:16 +0100

    Update RaceDay.js

--- RaceDay.js
+++ RaceDay.js
@@ -1,21 +1,22 @@
-let raceNumber = Math.floor(Math.random() * 1000);
 
-let early = true;
-let age = 45;
+def raceDay(age): 
+  let raceNumber = Math.floor(Math.random() * 1000);
+  let early = true;
+  let age = 0;
 
-if (early && age > 18) {
-  raceNumber += 1000;
-}
+  if (early && age > 18) {
+    raceNumber += 1000;
+    return `Your race will be at 9:30am and your race number is: ${raceNumber}.`;
+  } 
+  else if (!early && age > 18) {
+    return `Your race will be at 11:00am and you race number is: ${raceNumber}.`;
+  }
+  else if (age < 18) {
+    return `Your race will be at 12:30pm and your race number is: ${raceNumber}.`;
+  }
+  else {
+    return 'Please approach the registration desk, thanks!';
+  }
 
-if (early && age > 18) {
-  console.log(`Your race will be at 9:30am and your race number is: ${raceNumber}.`);
-}
-else if (!early && age > 18) {
-  console.log(`Your race will be at 11:00am and you race number is: ${raceNumber}.`);
-}
-else if (age < 18) {
-  console.log(`Your race will be at 12:30pm and your race number is: ${raceNumber}.`);
-}
-else {
-  console.log('Please approach the registration desk, thanks!');
-}
+
+raceDay();
